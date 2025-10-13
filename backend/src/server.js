c7c1b5aed096b99d1139b0fe97a759fded7ff99b
tinykofi <kofiasare163@gmail.com>
2025-10-13 16:32:53 +0000

    Add Message Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-// import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV == "production") {
