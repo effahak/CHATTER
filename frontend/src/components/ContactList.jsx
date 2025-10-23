ff6a1a5155cc76f824cb533d9c757b424f87473a
tinykofi <kofiasare163@gmail.com>
2025-10-23 17:44:15 +0000

    Add online/offline status to ChatHeader, ChatsList, and ContactList components

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
 import NoChatsFound from "./NoChatsFound";
+import { useAuthStore } from "../store/useAuthStore";
 
 function ChatsList() {
   const { getAllContacts, allContacts, isUsersLoading, setSelectedUser } =
     useChatStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getAllContacts();
@@ -16,23 +18,25 @@ function ChatsList() {
 
   return (
     <>
-      {allContacts.map((chat) => (
+      {allContacts.map((contact) => (
         <div
-          key={chat._id}
+          key={contact._id}
           className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
-          onClick={() => setSelectedUser(chat)}
+          onClick={() => setSelectedUser(contact)}
         >
           <div className="flex items-center gap-3">
-            <div className={`avatar online`}>
+            <div
+              className={`avatar ${onlineUsers.includes(contact._id) ? "online" : "offline"}`}
+            >
               <div className="size-12 rounded-full">
                 <img
-                  src={chat.profilePic || "assets/avatar.png"}
-                  alt={chat.fullName}
+                  src={contact.profilePic || "assets/avatar.png"}
+                  alt={contact.fullName}
                 />
               </div>
             </div>
             <h4 className="text-slate-200 font-medium truncate">
-              {chat.fullName}
+              {contact.fullName}
             </h4>
           </div>
         </div>
